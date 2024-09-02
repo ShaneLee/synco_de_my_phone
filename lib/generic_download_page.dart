37c00f056dbe78776903156873a84e858ce82e2e
Shane <me@shanel.ee>
2024-09-02 07:10:01 +0100

    Make generic downloads show the count of not downloaded

--- lib/generic_download_page.dart
+++ lib/generic_download_page.dart
@@ -12,6 +12,7 @@ class GenericDownloadPage extends StatefulWidget {
 class _GenericDownloadPageState extends State<GenericDownloadPage> {
   Downloader downloader = Downloader();
   List<FileStatus> downloadedFiles = [];
+  int notDownloadedCount = 0;
 
   @override
   void initState() {
@@ -25,22 +26,35 @@ class _GenericDownloadPageState extends State<GenericDownloadPage> {
 
   Future<void> _download() async {
     final ebooks = await downloader.downloadFilesFromFolder(
-        '${Config.server}/synco/books/books-to-sync', '/storage/emulated/0/kindle/', {'.mobi'});
+        '${Config.server}/synco/books/books-to-sync',
+        '/storage/emulated/0/kindle/',
+        {'.mobi'});
     final music = await downloader.downloadDirectoriesFromFolder(
         '${Config.server}/synco/music/', '/storage/sdcard1/Music/', {'.mp3'});
     final audiobooks = await downloader.downloadDirectoriesFromFolder(
-        '${Config.server}/synco/audiobooks/', '/storage/sdcard1/Audiobooks/', {".mp3", ".mp4", ".m4a"});
+        '${Config.server}/synco/audiobooks/',
+        '/storage/sdcard1/Audiobooks/',
+        {".mp3", ".mp4", ".m4a"});
 
     setState(() {
-      downloadedFiles.addAll(ebooks);
-      downloadedFiles.addAll(music);
-      downloadedFiles.addAll(audiobooks);
+      downloadedFiles = [...ebooks, ...music, ...audiobooks];
+
+      // Filter the list to only include files with a status of 'success'
+      downloadedFiles =
+          downloadedFiles.where((file) => file.status == 'success').toList();
+
+      // Count the number of files that were not downloaded successfully
+      notDownloadedCount = ebooks.length +
+          music.length +
+          audiobooks.length -
+          downloadedFiles.length;
     });
   }
 
   Future<void> _refreshFiles() async {
     setState(() {
       downloadedFiles.clear();
+      notDownloadedCount = 0;
     });
     await _download();
   }
@@ -53,23 +67,37 @@ class _GenericDownloadPageState extends State<GenericDownloadPage> {
       ),
       body: RefreshIndicator(
         onRefresh: _refreshFiles,
-        child: ListView.builder(
-          itemCount: downloadedFiles.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(
-                downloadedFiles[index].savePath,
+        child: Column(
+          children: [
+            // Display the count of files that weren't downloaded successfully
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Files that were not downloaded: $notDownloadedCount',
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
-              subtitle: Text(
-                downloadedFiles[index].status == 'success'
-                    ? downloadedFiles[index].status
-                    : '${downloadedFiles[index].status} - ${downloadedFiles[index].reason ?? ''}',
-                style: TextStyle(
-                  color: downloadedFiles[index].status == 'success' ? Colors.black : Colors.red,
-                ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: downloadedFiles.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      downloadedFiles[index].savePath,
+                    ),
+                    subtitle: Text(
+                      downloadedFiles[index].status,
+                      style: TextStyle(
+                        color: downloadedFiles[index].status == 'success'
+                            ? Colors.black
+                            : Colors.red,
+                      ),
+                    ),
+                  );
+                },
               ),
-            );
-          },
+            ),
+          ],
         ),
       ),
     );
